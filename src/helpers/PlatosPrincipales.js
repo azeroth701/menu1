aab91c4ca1cd2f50b595ac4657c76524c7bae5c2
azeroth <jefry43429@gmail.com>
2023-06-29 18:14:43 -0600

    wip refactor list minimizar code

--- src/helpers/PlatosPrincipales.js
+++ src/helpers/PlatosPrincipales.js
@@ -32,9 +32,106 @@ export const FoodCardList = [
         "Crujientes rebanadas de pechuga de pollo preparadas en una salsa dulce y servidas sobre fideos de arroz fritos. (1420 CAL) 18.5 Crispy chicken breast slices prepared in a sweet sauce and served over fried rice noodles. (1420 CAL) 18.5"
     },
     {
-        img:  se,
+      img:  se,
+      title: "SESAME CHICKEN",
+      description:
+        "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+    },
+    {
+        img:  sw,
+        title: "SWEET & SOUR CHICKEN  ",
+        description:
+          "Crujientes rebanadas de pechuga de pollo, combinadas con trocitos de piÃ±a, pimientos verdes, cebolla y salsa agridulce. (960 CAL) 17.5 Crispy chicken breast slices, combined with pineapple chunks, green peppers, onion, and sweet and sour sauce. (960 CAL) 17.5"
+      },
+      {
+        img:  gi,
+        title: "GINGER CHICKEN WITH BROCCOLI",
+        description:
+          " Rebanadas de pechuga de pollo salteadas en cebollines y jengibre, rodeadas de brÃ³coli. (530 CAL) 18 Chicken breast slices sautÃ©ed with chives and ginger; steamed broccoli. (530 CAL) 18"
+      },
+      {
+        img:  pa,
+        title: "KUNG PAO CHICKEN",
+        description:
+          "Pechuga de pollo ligeramente empanada, salteadas en chiles, manÃ­ y salsa kung pao. (1090 CAL) 18 Lightly breaded chicken breast sautÃ©ed with chili peppers, peanuts and kung pao sauce. (1090 CAL) 18"
+      },
+      {
+        img:  or,
+        title: "ORANGE CHICKEN",
+        description:
+          "Tiernas y crujientes rebanadas de pechuga de pollo preparadas en una salsa ligeramente picante de chile junto a cÃ¡scaras de naranja. (1000 CAL) 18.5  Tender and crispy chicken breast slices prepared in a slightly spicy chili sauce and orange peels (1000 CAL) 18.5 "
+      },
+      {
+        img:  be,
+        title: "MONGOLIAN BEEF",
+        description:
+          "Rebanadas de carne de res caramelizadas y salteadas con cebollines y ajo. (800 CAL) 23  Sliced beef in sweet soy glaze, garlic, snipped green onion. (800 CAL) 23"
+      },
+      {
+        img:  wk,
+        title: "WOK-CHARRED BEEF",
+        description:
+          "Rebanadas de carne de res salteadas con setas, chiles, ajo y pimientos rojos y verdes. (890 CAL) 22 Sliced beef sautÃ©ed with mushrooms, chili peppers, garlic, and red and green bell peppers. (890 CAL) 22 "
+      },
+      {
+        img:  tr,
+        title: "TRADITIONAL PEPPER STEAK",
+        description:
+          "Rebanadas de carne de res salteadas con cebollas, pimientos rojos y verdes. PruÃ©balo picante. (680 CAL) 22  Sliced beef sautÃ©ed with onions, green and red bell peppers. Try our spicy version. (680 CAL) 22"
+      },
+      {
+        img:  br,
+        title: "BEEF WITH BROCCOLI",
+        description:
+          "Rebanadas de carne de res salteadas con jengibre,cebollines y ajo. Mezcladas con brÃ³coli. (740 CAL) 22  Sliced beef sautÃ©ed with ginger, chives and garlic, steamed broccoli. (740 CAL) 22"
+      },
+      {
+        img:  lo,
         title: "SESAME CHICKEN",
         description:
           "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
       },
+      {
+        img:  cr,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      {
+        img:  mp,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      {
+        img:  gl,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      {
+        img:  ml,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      {
+        img:  fe,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      {
+        img:  po,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      {
+        img:  ri,
+        title: "SESAME CHICKEN",
+        description:
+          "Rebanadas de pechuga de pollo, brÃ³coli, pimientos rojos, y cebollas en una salsa de ajonjolÃ­ ligeramente picante. (930 CAL) 18.5  Sliced chicken breasts, broccoli, red bell peppers, and onions in a slightly spicy sesame sauce. (930 CAL) 18.5 "
+      },
+      
   ];
